f62ca5d96ca148a139fa8c8b6354b1aa7d6d0afe
firyal-salsa <53255114+firyal-salsa@users.noreply.github.com>
2021-10-29 08:59:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ server.use(express.urlencoded({ extended: true }))
 server.use("/public", express.static("public"))
 server.use(main)
 
-server.get('api', function(req, res, next) {
+server.get('api/product/produk_harga', function(req, res, next) {
     res.json({msg: 'this is CORS-enable for all origins!'})
 })
 
